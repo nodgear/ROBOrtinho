0719c06684b648f4100bda62ce4bac466edf1dd2
Nodge <matheus.silva3@ftc.edu.br>
2021-03-25 00:18:58 -0300

    Moving tempvoice event to utils and callling it as utility

--- src/events/voiceStateUpdate.js
+++ src/events/voiceStateUpdate.js
@@ -2,72 +2,7 @@ const config = require('../config.json');
 const utils  = require('../utils/util.js');
 const { i18n }   = require('../i18n/i18n.js');
 const log    = require('../utils/log.js');
-const { generateSlug }  = require('random-word-slugs');
 
-let voiceInvites = new Map();
-
-
-function deleteEmptyChannelAfterDelay(voiceChannel, delayMS = 12000, callback){
-	if(!voiceChannel) return;
-	if(voiceChannel.members.first()) return;
-	if(!voiceChannel.health) voiceChannel.health = 0;
-	voiceChannel.health += 1;
-	setTimeout(function(){	//queue channel for deletion and wait
-		if(!voiceChannel) return;
-		if(voiceChannel.members.first()) return;
-		voiceChannel.health -= 1;
-		if(voiceChannel.health > 0) return;
-		voiceChannel.delete()	//delete channel
-            .then( callback )
-			.catch(error => console.log(error));
-	}, delayMS);
-}
-
-async function createChannel( client, channel, user ) {
-    // Database is not ready or doesn't exist... try again, i guess?
-    if (!client.dynamicVC) {
-        log.error("Initialized createChannel, but the dynamic voice database is dead!")
-        return;
-    }
-
-    // This may be harcoded, but this a feature written only for the uni discord. So we are also checking if we are in the right guild.
-    if (!channel.guild.id == config.guildID)
-        return;
-
-    // Guess what? the hardcode is back, baby!
-    const category = client.channels.cache.get('824426616121851964');
-    const guild = channel.guild;
-
-    switch (channel.name) {
-        case "Criar Público":
-            let channel = client.dynamicVC.get(`${user.user.id}`).createdChannel
-            if (channel){
-                user.send("Hey! Você não pode criar duas salas ao mesmo tempo :(\nExclua o canal que você já criou para criar um novo");
-                return
-            }
-
-            const pChannel = await guild.channels.create(generateSlug(), { type: 'voice', parent: category });
-
-            deleteEmptyChannelAfterDelay(pChannel, 12000, () => {
-                client.dynamicVC.delete(`${user.user.id}`)
-            });
-
-            user.voice.setChannel(pChannel);
-
-            client.dynamicVC.set(`${user.user.id}`).createdChannel
-
-            log.debug(`User create voice chat, public ${channel}`)
-
-            break;
-        case "Criar Privado":
-            break;
-        default:
-            deleteEmptyChannelAfterDelay(channel, 12000, () => {
-                client.dynamicVC.delete(`${user.user.id}`)
-            });
-            return;
-    }
-}
 
 async function event(client, oldState, newState) {
     const user = newState.member.user
@@ -80,22 +15,13 @@ async function event(client, oldState, newState) {
 
     if (oldVC === null) {
         log.voice('STATE', user, newVC, "Joined");
-        createChannel( client, newVC, newState.member )
+
     } else if (newVC === null) {
         log.voice('STATE', user, oldVC, "Disconnected from");
     } else {
         log.voice('STATE', user, newVC, "Moved to");
-        createChannel( client, newVC, newState.member )
+
     }
-
-    if (!client.dynamicVC)
-        return;
-
-    client.dynamicVC.ensure(`${user.id}`, {
-        user: user.id,
-        createdChannel: false
-    });
-
 
 }
 
---
+++ src/utils/tempvoice.js
@@ -0,0 +1,84 @@
+module.exports = {
+    autoCreateChannel: async (client, options) => {
+        class setting {
+            constructor() {
+                this.options = options;
+                this.userLimit = (options && options.userLimit) || 17;
+                this.reason = (options && options.reason) || "Powered by @ds112";
+                this.nameStartsWith = (options && options.nameStartsWith) || "+ ";
+                this.nameStartsWithTemp = (options && options.nameStartsWithTemp) || "* ";
+            }
+        }
+        let settings = new setting()
+        client.on('voiceStateUpdate', (oldMember, newMember) => {
+            // ! leave
+            let isLeave = (oldMember.channel != undefined)
+
+            if (isLeave) {
+                console.log(isLeave);
+                if (oldMember.channel.name.startsWith(settings.nameStartsWithTemp)) {
+                    if (oldMember.channel.members.size == 0) {
+                        oldMember.channel.delete()
+                    }
+                    else { // change name
+                        let matchMember = oldMember.channel.members.find(x => `${settings.nameStartsWithTemp}${x.displayName}` == oldMember.channel.name);
+                        if (matchMember == null) {
+                            oldMember.channel.setName(`${settings.nameStartsWithTemp}${oldMember.channel.members.random().displayName}`)
+                        }
+                    }
+                }
+            }
+
+            // todo create channel
+            if (newMember.channel != null && newMember.channel.name.startsWith(settings.nameStartsWith)) {
+                newMember.guild.channels.create(`${settings.nameStartsWithTemp}${newMember.member.user.username}`, {
+                    type: "voice",
+                    parent: newMember.channel.parent,
+                    userLimit: settings.userLimit,
+                    reason: settings.reason
+                }).then(cloneChannel => newMember.setChannel(cloneChannel))
+            }
+        });
+
+    },
+
+    autoCreateChannelOnTop: async (client, options) => {
+        class setting {
+            constructor() {
+                this.options = options;
+                this.userLimit = (options && options.userLimit) || 17;
+                this.reason = (options && options.reason) || "Powered by @ds112";
+                this.nameStartsWith = (options && options.nameStartsWith) || "+ ";
+                this.nameStartsWithTemp = (options && options.nameStartsWithTemp) || "* ";
+            }
+        }
+        let settings = new setting()
+        client.on('voiceStateUpdate', (oldMember, newMember) => {
+            // ! leave
+            let isLeave = (oldMember.channel != undefined)
+            if (isLeave) {
+                if (oldMember.channel.name.startsWith(settings.nameStartsWithTemp)) {
+                    if (oldMember.channel.members.size == 0) {
+                        oldMember.channel.delete()
+                    }
+                    else { // change name
+                        let matchMember = oldMember.channel.members.find(x => `${settings.nameStartsWithTemp}${x.displayName}` == oldMember.channel.name);
+                        if (matchMember == null) {
+                            oldMember.channel.setName(`${settings.nameStartsWithTemp}${oldMember.channel.members.random().displayName}`)
+                        }
+                    }
+                }
+            }
+
+            // todo create channel
+            if (newMember.channel != null && newMember.channel.name.startsWith(settings.nameStartsWith)) {
+                newMember.guild.channels.create(`${settings.nameStartsWithTemp}${newMember.member.user.username}`, {
+                    type: "voice",
+                    userLimit: settings.userLimit,
+                    reason: settings.reason
+                }).then(cloneChannel => newMember.setChannel(cloneChannel))
+            }
+        });
+
+    }
+}
