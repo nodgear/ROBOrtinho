eb483c0bffdb4cc0a0f048a8b00acbd78bf6aac8
Nodge <matheus.silva3@ftc.edu.br>
2021-03-25 01:03:07 -0300

    Made private channels override

--- src/utils/tempvoice.js
+++ src/utils/tempvoice.js
@@ -1,10 +1,10 @@
 module.exports = {
-    autoCreateChannel: async (client, options) => {
+    autoCreateChannelPrivate: async (client, options) => {
         class setting {
             constructor() {
                 this.options = options;
                 this.userLimit = (options && options.userLimit) || 17;
-                this.reason = (options && options.reason) || "Powered by @ds112";
+                this.reason = (options && options.reason) || "Canal temporário";
                 this.nameStartsWith = (options && options.nameStartsWith) || "+ ";
                 this.nameStartsWithTemp = (options && options.nameStartsWithTemp) || "* ";
             }
@@ -15,7 +15,7 @@ module.exports = {
             let isLeave = (oldMember.channel != undefined)
 
             if (isLeave) {
-                console.log(isLeave);
+                // console.log(isLeave);
                 if (oldMember.channel.name.startsWith(settings.nameStartsWithTemp)) {
                     if (oldMember.channel.members.size == 0) {
                         oldMember.channel.delete()
@@ -29,14 +29,26 @@ module.exports = {
                 }
             }
 
-            // todo create channel
             if (newMember.channel != null && newMember.channel.name.startsWith(settings.nameStartsWith)) {
+                const everyone = newMember.guild.roles.everyone;
                 newMember.guild.channels.create(`${settings.nameStartsWithTemp}${newMember.member.user.username}`, {
                     type: "voice",
                     parent: newMember.channel.parent,
                     userLimit: settings.userLimit,
-                    reason: settings.reason
-                }).then(cloneChannel => newMember.setChannel(cloneChannel))
+                    reason: settings.reason,
+                    permissionOverwrites: [
+                        {
+                            id: everyone.id,
+                            deny: ['CONNECT'],
+                        },
+                        {
+                            id: newMember.member.user.id,
+                            allow: ['CONNECT', 'MUTE_MEMBERS', 'DEAFEN_MEMBERS', 'CONNECT', 'MOVE_MEMBERS']
+                        }
+                    ]
+                })
+                .then(cloneChannel => newMember.setChannel(cloneChannel))
+                .catch( console.log() )
             }
         });
 
@@ -47,9 +59,9 @@ module.exports = {
             constructor() {
                 this.options = options;
                 this.userLimit = (options && options.userLimit) || 17;
-                this.reason = (options && options.reason) || "Powered by @ds112";
+                this.reason = (options && options.reason) || "Temporario";
                 this.nameStartsWith = (options && options.nameStartsWith) || "+ ";
-                this.nameStartsWithTemp = (options && options.nameStartsWithTemp) || "* ";
+                this.nameStartsWithTemp = (options && options.nameStartsWithTemp) || "* "
             }
         }
         let settings = new setting()
@@ -62,7 +74,9 @@ module.exports = {
                         oldMember.channel.delete()
                     }
                     else { // change name
-                        let matchMember = oldMember.channel.members.find(x => `${settings.nameStartsWithTemp}${x.displayName}` == oldMember.channel.name);
+                        let matchMember = oldMember.channel.members.find(x => `${settings.nameStartsWithTemp}${x.displayName}` == oldMember.channel.name)
+                        .catch( console.log() )
+                        ;
                         if (matchMember == null) {
                             oldMember.channel.setName(`${settings.nameStartsWithTemp}${oldMember.channel.members.random().displayName}`)
                         }
@@ -76,7 +90,65 @@ module.exports = {
                     type: "voice",
                     userLimit: settings.userLimit,
                     reason: settings.reason
-                }).then(cloneChannel => newMember.setChannel(cloneChannel))
+                })
+                .then(cloneChannel => newMember.setChannel(cloneChannel))
+                .catch( console.log() )
+
+            }
+        });
+
+    },
+
+    autoCreateChannelPublic: async (client, options) => {
+        class setting {
+            constructor() {
+                this.options = options;
+                this.userLimit = (options && options.userLimit) || 17;
+                this.reason = (options && options.reason) || "Canal temporário";
+                this.nameStartsWith = (options && options.nameStartsWith) || "+ ";
+                this.nameStartsWithTemp = (options && options.nameStartsWithTemp) || "* ";
+            }
+        }
+        let settings = new setting()
+        client.on('voiceStateUpdate', (oldMember, newMember) => {
+            // ! leave
+            let isLeave = (oldMember.channel != undefined)
+
+            if (isLeave) {
+                // console.log(isLeave);
+                if (oldMember.channel.name.startsWith(settings.nameStartsWithTemp)) {
+                    if (oldMember.channel.members.size == 0) {
+                        oldMember.channel.delete()
+                    }
+                    else { // change name
+                        let matchMember = oldMember.channel.members.find(x => `${settings.nameStartsWithTemp}${x.displayName}` == oldMember.channel.name);
+                        if (matchMember == null) {
+                            oldMember.channel.setName(`${settings.nameStartsWithTemp}${oldMember.channel.members.random().displayName}`)
+                        }
+                    }
+                }
+            }
+
+            if (newMember.channel != null && newMember.channel.name.startsWith(settings.nameStartsWith)) {
+                const everyone = newMember.guild.roles.everyone;
+                newMember.guild.channels.create(`${settings.nameStartsWithTemp}${newMember.member.user.username}`, {
+                    type: "voice",
+                    parent: newMember.channel.parent,
+                    userLimit: settings.userLimit,
+                    reason: settings.reason,
+                    permissionOverwrites: [
+                        {
+                            id: everyone.id,
+                            deny: ['CONNECT'],
+                        },
+                        {
+                            id: newMember.member.user.id,
+                            allow: ['CONNECT', 'MUTE_MEMBERS', 'DEAFEN_MEMBERS', 'CONNECT', 'MOVE_MEMBERS']
+                        }
+                    ]
+                })
+                .then(cloneChannel => newMember.setChannel(cloneChannel))
+                .catch( console.log() )
             }
         });
 
