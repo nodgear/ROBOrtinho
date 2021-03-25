877360eb6e060a6091b6458d1939ee91ca3b20d4
Fabriicio Vello <fabriciovello@gmail.com>
2021-03-24 22:06:10 -0300

    set standard role to new member

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -4,9 +4,10 @@ const i18n    = require('../i18n/i18n.js');
 const log     = require('../utils/log.js');
 
 async function event(client, member) {
-    log.debug("Evento rodou", "GUILD MEMBER ADD")
-    member.send("Your message here.");
+    member.send("Bem vindo ao grupo de sistemas, por favor registre-se nas materias que você esta matriculado(a) no canal registro, sempre que precisar consulte as regras");
 
+    let role = message.guild.roles.find(role => role.id === "824432846693335070");
+    member.addRole(role);
 }
 
 module.exports = event;
